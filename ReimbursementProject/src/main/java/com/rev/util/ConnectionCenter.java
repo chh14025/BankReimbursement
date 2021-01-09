71d96d0b6a3011d0b39156e4adf8fb01d4dea01b
will.h <64234764+chh14025@users.noreply.github.com>
2021-01-09 00:11:25 -0500

    removed info

--- ReimbursementProject/src/main/java/com/rev/util/ConnectionCenter.java
+++ ReimbursementProject/src/main/java/com/rev/util/ConnectionCenter.java
@@ -5,60 +5,10 @@ import java.sql.*;
 
 public class ConnectionCenter {
 	
-//	private static Connection conn;
-//	
-//	public static Connection getConnection() {
-//		
-//		Connection con = null;
-//	    if (System.getenv("planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com") != null) {
-//	      try {
-//	      Class.forName("org.postgresql.Driver");
-//	      String dbName = "";
-//	      String userName = System.getenv("postgres");
-//	      String password = System.getenv("jowill7!");
-//	      String hostname = System.getenv("planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com");
-//	      String port = System.getProperty("5432");
-//	      String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-//	      System.out.println("Getting remote connection with connection string from environment variables.");
-//	      con = DriverManager.getConnection(jdbcUrl);
-//	      System.out.println("Remote connection successful.");
-//	      conn = con;
-//	    }
-//	    catch (ClassNotFoundException e) { System.out.println(e.toString());}
-//	    catch (SQLException e) { System.out.println(e.toString());}
-//	    }
-//	    return conn;
-//	  }
-//		
-//		public static Connection getConnection() {
-//		    if (System.getenv("planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com") != null) {
-//		      try {
-//		      Class.forName("org.postgresql.Driver");
-//		      String dbName = "";
-//		      String userName = System.getenv("postgres");
-//		      String password = System.getenv("jowill7!");
-//		      String hostname = System.getenv("planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com");
-//		      String port = System.getenv("6543");
-//		      String jdbcUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + dbName + "?user=" + userName + "&password=" + password;
-//		      System.out.println("Getting remote connection with connection string from environment variables.");
-//		      Connection con = DriverManager.getConnection(jdbcUrl);
-//		      System.out.println("Remote connection successful.");
-//		      conn = con;
-//		    }
-//		    catch (ClassNotFoundException e) { System.out.println(e.toString());}
-//		    catch (SQLException e) { System.out.println(e.toString());}
-//		    }
-//		    return conn;
-//		  }
-	
-//	private static final String URL = "jdbc:postgresql://planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com/postgres";
-//	private static final String USERNAME = "postgres";
-//	private static final String PASSWORD = "jowill7!";
-	
 	private static Connection conn;
-	private static final String URL = "jdbc:postgresql://planet-db.cajbwopfnhwq.us-east-2.rds.amazonaws.com/postgres";
-	private static final String USERNAME = "postgres";
-	private static final String PASSWORD = "jowill7!";
+	private static final String URL = "";
+	private static final String USERNAME = "";
+	private static final String PASSWORD = "";
 	
 	
 	public static Connection getConnection() {
